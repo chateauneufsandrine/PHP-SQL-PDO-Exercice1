23d627d4992e8e0065b05efabb422bb86e366a8e
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-04-23 15:11:29 +0200

    commit

--- public/exercice2.php
+++ public/exercice2.php
@@ -1,38 +1,30 @@
-<?php
-// Exercice 1
-// Afficher tous les clients.
+<?php 
+// Exercice 2
+// Afficher tous les types de spectacles possibles.
 
 require_once "../utils/db_connexion.php";
 
 // on commence par préparer la requète grace à query(requête sans variable)
-$request =  $db->query('SELECT clients.lastName, clients.firstName 
-FROM clients');
+$request =  $db->query('SELECT showtypes.type 
+FROM showtypes');
 
 // on récupère la réponse à la requète grâce à fetchAll(), car j'ai pls livres en BDD
-$clients = $request->fetchAll(PDO::FETCH_ASSOC);
-// var_dump($clients);
+$showtypes = $request->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($showtypes);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
-
-    <h1>Liste de clients du Colyseum</h1>
-    <ol><?php foreach ($clients as $client) {  ?>
-            <li><?= $client["lastName"] . ", " . $client["firstName"] ?></li>
-        <?php } ?>
-    </ol>
-
-
-
+    <h1>Liste de tous les spectacle de Colyseum</h1>
+  <p><?php foreach ($showtypes as $showtype) {?>
+        <?= $showtype["type"] . ", " ?>
+<?php } ?></p>
 </body>
-
 </html>
